a7fcf20c82358efc0dc75162000d6cb335c5c307
arzunuvar <62670222+arzunuvar@users.noreply.github.com>
2021-02-19 19:38:05 +0300

    odev4

--- SQLQuery.php
+++ SQLQuery.php
@@ -1,19 +1,36 @@
 <?php
 
-trait SQLCrud {
-      public function create(SQLQuery $query) {
+
+class SQLQuery extends SQL implements IQuery {
+      use SQLCrud;
+      private $table;
+      private $bindings;
+      private $offset;
+      private $limit;
+      private $wheres;
+      private $action;
+
+      public function setTable(string $table) {
+
+            return $this;
+      }
+      public function addBinding(string $key, string $value) {
+            return $this;
+      }
+      public function removeBinding(string $key) {
+            return $this;
+      }
+      public function setPaginate(int $limit, int $offset) {
+            return $this;
+      }
+      public function addWhere(string $column, string $operator, string $value) {
+            return $this;
+      }
+      public function select() {
+            return $this;
+      }
+      public function compile() {
             
       }
-      public function update(SQLQuery $query) {
-
-      }
-      public function delete(SQLQuery $query) {
-
-      }
-      public function get(SQLQuery $query) {
-
-      }
-      public function first(SQLQuery $query) {
-
-      }
-}
+      
+  }
