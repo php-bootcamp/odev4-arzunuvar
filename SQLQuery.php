265b00343ebc89d128b1d17655a8212202c9237f
arzunuvar <62670222+arzunuvar@users.noreply.github.com>
2021-02-19 20:49:36 +0300

    odev4

--- SQLQuery.php
+++ SQLQuery.php
@@ -1,37 +1,49 @@
 <?php
- class SQLQuery{
-    private string $table;
-    private array $bindings;
-    private int $offset;
-    private int $limit;
-    private array $wheres;
-    private string $action;
 
-    public function setTable(string $table){
-        return $this;
-    }
-    public function addBinding (string $key, string $value) {
-        return $this;
-    }
-    public function removeBinding (string $key) {
-        return $this;
-    }
-    public function setPaginate (int $limit, int $offset) {
-        return $this;
-    }
-    public function addWhere (string $column, string $operator, string $value) {
-        return $this;
-    }
-    public function delete(){
+class SQLQuery  {
 
-    }
-    public function update(){
-        
-    }
-    public function select(){
-        return $this;
-    }
-    public function compile(){
-        
-    }
+  private string $table;
+  private array $bindings;
+  private int $offset;
+  private int $limit;
+  private array $wheres;
+  private string $action;
+
+  public function setTable(string $table) {
+    return $this;
+  }
+
+  public function addBinding(string $key, string $value) {
+    return $this;
+  }
+
+  public function removeBinding(string $key) {
+    return $this;
+  }
+
+  public function setPaginate(int $limit, int $offset) {
+    return $this;
+  }
+
+  public function addWhere(string $column, string $operator, string $value) {
+    return $this;
+  }
+
+  public function delete() {
+    return $this;
+  }
+
+  public function update() {
+    return $this;
+  }
+
+  public function select() {
+    return $this;
+  }
+
+  public function complie() {
+    return $this;
+  }
+
+
 }
